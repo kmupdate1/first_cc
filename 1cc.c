c2ed24e2ebe1cad0b2bd3db9ba7623603bf2b1a1
Ken <lax.potcx1562@gmail.com>
2020-03-29 02:59:48 +0900

    while modifiding at the momentgit add --all!

--- 1cc.c
+++ 1cc.c
@@ -22,6 +22,25 @@ struct Token {
 	char *str;      // トークン文字列
 };
 
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+	NodeKind kind; // ノードの辺
+	Node *lhs;     // 左辺
+	Node *rhs;     // 右辺
+	int val;       // kindがND_NUMの場合のみ使う
+};
+
 // 入力プログラム
 char *user_input;
 
@@ -125,6 +144,57 @@ Token *tokenize(char *p) {
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node -> kind = kind;
+	node -> lhs = lhs;
+	node -> rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node -> kind = ND_NUM;
+	node -> val = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if ( consume('+') )
+			node = new_node(ND_ADD, node, mul());
+		else if ( consume('-') )
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for (;;) {
+		if ( consume('*') )
+			node = new_node(ND_MUL, node, primary());
+		else if ( consume('/') )
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *primary() {
+	if ( consume('(') ) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
